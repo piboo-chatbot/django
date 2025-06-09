6e8dc0e8aef56428a559cf433166f08c35f6448e
Ohjunghh <cmt5ttt@gmail.com>
2025-06-09 14:53:34 +0900

    로그인/회원가입/챗봇/마이페이지

--- backend/chatbot/urls.py
+++ backend/chatbot/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import AskView, chatbot_page, intro_view
+from .views import AskView, chatbot_page, intro_view, mypage_view
 
 
 urlpatterns = [
     path('ask/', AskView.as_view(), name='ask'),        # 질문 처리 API
     path('', chatbot_page, name='chatbot'),             # 챗봇 화면
     path('intro/', intro_view, name = 'intro'),
+    path('mypage/', mypage_view, name='mypage'),
 ]
